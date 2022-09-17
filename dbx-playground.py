5e317523fed5bca289c615230d13bf0c1d3bc4d1
carlos-7@hotmail.de <carlos-7@hotmail.de>
2022-09-17 13:21:53 +0000

    Initial commit
    Exploring dbx.

---
+++ dbx-playground.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Notbooks Introduction
+# MAGIC * UI Intro
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "Hello world"
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC var msg = "Hello world"
+
+# COMMAND ----------
+
+msg = "This is cool"
+print(msg)
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
+
+# COMMAND ----------
+
+
